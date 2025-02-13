81c46f2a9941b3ef3b2d5a45fb3efad98b997a8e
Jiwan Ye <rnfls2012@gmail.com>
2025-02-14 00:51:19 +0900

    feat: 주문완료 페이지 내 `purchase` 이벤트 호출 시 할인금액, 배송비 항목 추가

--- src/plugins/gtm.js
+++ src/plugins/gtm.js
@@ -46,17 +46,48 @@ export default {
 
             // 구매완료
             logPurchase(orderInfo) {
+                const gradePrice = orderInfo.gradePrice || 0 // 등급할인가격
+                const couponPrice = orderInfo.couponPrice || 0 // 쿠폰할인가격 (상품쿠폰할인인 경우에만)
+                const pointPrice = orderInfo.pointPrice || 0 // 포인트할인가격
+
+                const totalDiscount = gradePrice + couponPrice + pointPrice; // 할인금액의 총 합
+                const totalOrderValue = orderInfo.items.reduce((sum, item) => sum + (item.unitPrice * item.quantity), 0); // 주문내역에 포함되어 있는 상품 가격과 수량의 총 합 (할인금액은 계산에서 제외)
+
+                let items; // 주문 상품별 정보를 처리하기위해 미리 선언
+
+                // 할인이 없는 경우 바로 기본 정보만 포함하여 처리
+                if (totalDiscount === 0) {
+                    items = orderInfo.items.map(item => ({
+                        item_id: item.id,
+                        item_name: item.name,
+                        discount: 0,
+                        price: item.unitPrice, // 주문 당시 상품 가격
+                        quantity: item.quantity // 주문 당시 상품 갯수
+                    }));
+                } else {
+                    items = orderInfo.items.map(item => {
+                        const totalUnitPrice = item.unitPrice * item.quantity;
+                        const ratio = totalUnitPrice / totalOrderValue;
+                        const discount = Math.round(totalDiscount * ratio); // 주문의 전체 할인 금액별
+                        const discountPerItem = Math.round(discount / item.quantity); //
+
+                        return {
+                            item_id: item.id,
+                            item_name: item.name,
+                            discount: discountPerItem,
+                            price: item.unitPrice,
+                            quantity: item.quantity,
+                        }
+                    })
+                }
+
                 this.logEvent('purchase', {
                     ecommerce: {
                         transaction_id: orderInfo.orderNumber,
-                        value: orderInfo.totalAmount,
+                        value: totalOrderValue, //
                         currency: 'KRW',
-                        items: orderInfo.items.map(item => ({
-                            item_id: item.id,
-                            item_name: item.name,
-                            price: item.price * item.quantity,
-                            quantity: item.quantity
-                        }))
+                        shipping: orderInfo.shippingPrice, // 주문 배송비
+                        items: items
                     }
                 })
             }
